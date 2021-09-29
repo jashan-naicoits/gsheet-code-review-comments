14a0df9db5c95d21765f7c7e98b06e0ced63ca8b
vineethvijayan <vineeth.v@naicoits.com>
2021-09-29 16:55:42 +0530

    Fixed single API call

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-const GoogleSpreadsheet = require('google-spreadsheet');
-
+const GoogleSpreadsheet = require("google-spreadsheet");
 
 async function getNextCodeReviewId(fromGit, sheetTitle, sheetPos) {
   // fetch from github api
@@ -24,103 +23,95 @@ async function getNextCodeReviewId(fromGit, sheetTitle, sheetPos) {
   sheet.setHeaderProps(sheetPos.X, sheetPos.Y);
 
   const rows = await sheet.getRows();
-  
-  let lastCodeReviewId = ''
+
+  let lastCodeReviewId = "";
   for (let index = rows.length - 2; index > 0; index--) {
     const element = rows[index];
     if (element._rawData[0] != undefined) {
-      lastCodeReviewId = element._rawData[2]
+      lastCodeReviewId = element._rawData[2];
       // console.log(element._rawData[2]);
       break;
     }
   }
-  let nextCodeReviewId = ''
-  if(lastCodeReviewId != '') {
+  let nextCodeReviewId = "";
+  if (lastCodeReviewId != "") {
     // split string
-    const lastCodeReviewIdSplit = lastCodeReviewId.split('CD_RV_')
-    newCodeReviewId = Number(lastCodeReviewIdSplit[1])
+    const lastCodeReviewIdSplit = lastCodeReviewId.split("CD_RV_");
+    newCodeReviewId = Number(lastCodeReviewIdSplit[1]);
     // console.log('newCodeReviewId', newCodeReviewId);
-    nextCodeReviewId = 'CD_RV_' + String((newCodeReviewId + 1)).padStart(2, '0')
-
+    nextCodeReviewId = "CD_RV_" + String(newCodeReviewId + 1).padStart(2, "0");
+  } else {
+    nextCodeReviewId = "CD_RV_01";
   }
-  else {
-    nextCodeReviewId = 'CD_RV_01'
-  }
-  return nextCodeReviewId
+  return nextCodeReviewId;
 }
 
 // fetch from Github API
 async function fetchFromGithub(fromGit) {
-  //get main url
-  console.log('fetching github url', fromGit.gitUrl)
-  await fetch(fromGit.gitUrl, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${fromGit.token}`
+  let prData = [
+    {
+      commits_url: `${fromGit.gitUrl}/${fromGit.pr_number}/commits`,
+      review_comments_url: `${fromGit.gitUrl}/${fromGit.pr_number}/comments`,
     },
-  })
-  .then(response => response.json())
-  .then(async (data) => {
-    console.log('got data from github url');
-    console.log(data);
-    let prData = []
-    data.forEach((d) => {
-      console.log(d.number);
-      if(d.number == fromGit.pr_number) {
-        prData.push(d)
-        return
-      }
+  ];
+
+  // console.log(prData[0].review_comments_url);
+
+  fetchGitHubReviewComments(fromGit, prData).then(async (reviewer) => {
+    console.log("fetching commits url", prData[0].commits_url);
+
+    await fetch(`${prData[0].commits_url}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${fromGit.token}`,
+      },
     })
-    // console.log(data[0].commits_url);
-    // console.log(prData)
-    // commits_url
-
-    fetchGitHubReviewComments(fromGit, prData).then(async (reviewer) => {
-      console.log('fetching commits url')
-      await fetch(`${prData[0].commits_url}`, {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${fromGit.token}`
-        },
-      })
-      .then(response => response.json())
+      .then((response) => response.json())
       .then(async (data) => {
-        console.log('got commits url');
-        fetchGithubCommitDetails(fromGit, data, reviewer, reviewer.nextCodeReviewId);
+        console.log("got commits url");
+        fetchGithubCommitDetails(
+          fromGit,
+          data,
+          reviewer,
+          reviewer.nextCodeReviewId
+        );
       });
-
-    });
-
   });
-  
 }
 
-async function fetchGithubCommitDetails(fromGit, commits, reviewer, nextCodeReviewId) {
-  let statResult = []
-  let fileResult = []
-  let committer
-  console.log('fetching commit details url');
-  Promise.all(commits.map(async (commit) => {
-    res = await fetch(`${commit.url}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${fromGit.token}`
-      },
-    }).then(response => response.json())
-    .then(async (data) => {
-      committer = data.committer.login;
-      return {stat:data.stats, file:data.files}
+async function fetchGithubCommitDetails(
+  fromGit,
+  commits,
+  reviewer,
+  nextCodeReviewId
+) {
+  let statResult = [];
+  let fileResult = [];
+  let committer;
+  console.log("fetching commit details url");
+  Promise.all(
+    commits.map(async (commit) => {
+      res = await fetch(`${commit.url}`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${fromGit.token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then(async (data) => {
+          committer = data.committer.login;
+          return { stat: data.stats, file: data.files };
+        });
+      statResult.push(res.stat);
+      fileResult.push(res.file);
     })
-    statResult.push(res.stat)
-    fileResult.push(res.file)
-
-  })).then(async () => {
-    console.log('got commit details url combined');
+  ).then(async () => {
+    console.log("got commit details url combined");
     // console.log('statResult', statResult);
     // console.log(fileResult[0]);
 
-    let additions = 0
-    let deletions = 0
+    let additions = 0;
+    let deletions = 0;
     let changes = 0;
     let fileNames = [];
     // statResult.forEach((stat, index) => {
@@ -132,19 +123,21 @@ async function fetchGithubCommitDetails(fromGit, commits, reviewer, nextCodeRevi
 
     // console.log('fileResult', fileResult[0][0].sha);
     // console.log('fileResult', fileResult[fileResult.length - 1][0].sha);
-    let gitRevision = `${fileResult[0][0].sha}...${fileResult[fileResult.length - 1][0].sha}`
+    let gitRevision = `${fileResult[0][0].sha}...${
+      fileResult[fileResult.length - 1][0].sha
+    }`;
     // console.log('gitRevision', gitRevision);
     // console.log('fileResult', fileResult.length)
     fileResult.forEach((files, index) => {
       // console.log(files);
-      files.forEach(file => {
+      files.forEach((file) => {
         additions += file.additions;
         deletions += file.deletions;
         changes += file.changes;
 
         let fileName = file.filename;
         fileNames.push(fileName);
-      })
+      });
     });
 
     fileNames = new Set(fileNames);
@@ -155,59 +148,67 @@ async function fetchGithubCommitDetails(fromGit, commits, reviewer, nextCodeRevi
     // console.log('changes', changes);
     // console.log('fileNames', fileNames);
     const data = {
-      'Sprint': 'Sprint Name',
-      'TaskID': new Date(Date.now()).toLocaleString(),
-      'Code Review ID': nextCodeReviewId,
-      'Developer': committer,
-      'LOC Added': additions,
-      'LOC Modified': changes + deletions,
-      'Lines Of Code Reviewed': additions + changes + deletions,
-      'List of Files Reviewed': fileNames.join('\n'),
-      'Number of Comments': reviewer.commentsResult.length,
-      'Reviewer': reviewer.reviewer,
-      'Date of Review': new Date(Date.now()).toLocaleString(),
-      'GIT Revision': gitRevision,
-    }
+      Sprint: "Sprint Name",
+      TaskID: new Date(Date.now()).toLocaleString(),
+      "Code Review ID": nextCodeReviewId,
+      Developer: committer,
+      "LOC Added": additions,
+      "LOC Modified": changes + deletions,
+      "Lines Of Code Reviewed": additions + changes + deletions,
+      "List of Files Reviewed": fileNames.join("\n"),
+      "Number of Comments": reviewer.commentsResult.length,
+      Reviewer: reviewer.reviewer,
+      "Date of Review": new Date(Date.now()).toLocaleString(),
+      "GIT Revision": gitRevision,
+    };
 
-    accessSpreadSheet(fromGit, [data], 'CodeReviewSummary', {X: 'C', Y: 3});
+    accessSpreadSheet(fromGit, [data], "CodeReviewSummary", { X: "C", Y: 3 });
     // fetchGitHubReviewComments(fromGit);
-  })
+  });
 }
 
 async function fetchGitHubReviewComments(fromGit, data) {
-  let reviewer = ''
-  let commentsResult = null
-  let nextCodeReviewId = ''
+  let reviewer = "";
+  let commentsResult = null;
+  let nextCodeReviewId = "";
+  console.log("data[0].review_comments_url", data[0].review_comments_url);
   await fetch(data[0].review_comments_url, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Authorization': `Bearer ${fromGit.token}`
+      Authorization: `Bearer ${fromGit.token}`,
     },
   })
-  .then(response => response.json())
-  .then(async (data) => {
-    // console.log('done 2');
-    // console.log(data[0]);
-    // console.log(data[4]);
+    .then((response) => response.json())
+    .then(async (data) => {
+      // console.log('done 2');
+      // console.log(data);
+      // console.log(data[4]);
 
-    nextCodeReviewId = await getNextCodeReviewId(fromGit, 'CodeReviewSummary', {X: 'C', Y: 3});
+      nextCodeReviewId = await getNextCodeReviewId(
+        fromGit,
+        "CodeReviewSummary",
+        { X: "C", Y: 3 }
+      );
 
-    result = [];
-    data.forEach(d => {
-      // console.log(d.body, d.user.login, d.path);
-      reviewer = d.user.login;
-      result.push({
-        'Code Review ID': nextCodeReviewId,
-        'Comment': d.body,
-        'File Name': d.path,
+      result = [];
+      data.forEach((d) => {
+        // console.log(d.body, d.user.login, d.path);
+        reviewer = d.user.login;
+        result.push({
+          "Code Review ID": nextCodeReviewId,
+          Comment: d.body,
+          "File Name": d.path,
+        });
       });
+      commentsResult = result;
+      // console.log(result);
+      accessSpreadSheet(fromGit, result, "CodeReviewComments", {
+        X: "B",
+        Y: 5,
+      });
+      // return reviewer;
     });
-    commentsResult = result
-    // console.log(result);
-    accessSpreadSheet(fromGit, result, 'CodeReviewComments', {X: 'B', Y: 5});
-    // return reviewer;
-  });
-  return {reviewer, commentsResult, nextCodeReviewId};
+  return { reviewer, commentsResult, nextCodeReviewId };
 }
 
 async function accessSpreadSheet(fromGit, result, sheetTitle, sheetPos) {
@@ -228,7 +229,6 @@ async function accessSpreadSheet(fromGit, result, sheetTitle, sheetPos) {
   sheet.setHeaderProps(sheetPos.X, sheetPos.Y);
   // sheet.setHeaderProps('B', 5)
 
-  
   // sheet.addRow({
   //   'Code Review ID': Date.now(),
   //   'Comment': Date.now(),
@@ -240,7 +240,7 @@ async function accessSpreadSheet(fromGit, result, sheetTitle, sheetPos) {
   //   sheet.addRow(row) // this is a promise
   // });
 
-  //execute promise sequentially 
+  //execute promise sequentially
   // await Promise.all(result.map(row => sheet.addRow(row)))
   sheet.addRows(result);
 
@@ -249,34 +249,50 @@ async function accessSpreadSheet(fromGit, result, sheetTitle, sheetPos) {
   //     p.then(_ => sheet.addRow(row)),
   //   Promise.resolve()
   // )
-
 }
+
+
+fromGit = {
+  gitUrl: 'https://api.github.com/repos/vineethvijayan/workflow/pulls',
+  sheetId: '1V2vATD8aplBrz_VfgwZxu48q4t9vHJXfAtPIVWzQSa8',
+  client_email: 'codereview@codereviewtracker-296806.iam.gserviceaccount.com',
+  private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCKQ4ZMWvKzJn32\nJOUXJF1HyUaK7M/7oW0Uk6In7+aX58LL/s0R/RURoA9XFBAv1r18zBWxspqjOcVk\niUurCIeLfUKsMprftDn6ELmtGqFLtr9SD/Hd67cQ5vuRCQvN2w3TZGSpRYa8eXHB\n8IznHWvIAKkQGG0TSPsJo9Jh3u5/lgQLhbH+qgDUZtpb/bx1DJULmVzbKsVijZaT\nC927lk6vMj/BWXHFvqkwpuaXp5YCAR8AQp3wBOpXz7EykpL0WwRBVeotlmXEJlqg\nD1g1Sw3Pp3neUcHi0BEhyDF7I2b6JWCmLfOBmph4BU0xJwoE6o768QZToibrkn3R\nSC1th9V1AgMBAAECggEABjomU/gLUvBFNLnSq5PSs7H9VbJ2vObh/87qZbwTTvzR\niYMvVJkTwth0+GoJ6e2qWR2Ooz3fuGxDV1TWWJyou3/nvykEUAD7qic+BZgxHYox\n3GNQHxDyYiDVFwzTlZuWaCfvCgSnSIPREM1huzhYVgiu6e+qXKJT1B+nEQthAylQ\nj8txR2PQ76mjdBU+bau91U6rd2DEvnrMe88ecKgWVl9aZ8baXv4chZTMrGlOK57u\niVGa2dvA1A1RPvi5EAgHC4DP9CHfNzIhqFImVVxZVqTY8ZrTvT2O+Z7nL2rNv5KU\nqAJmQkWBMpUdKDnOenGVEpSyJxrTKSCQz/6Hbq5cYQKBgQDAcQG83+YwBlT4Kxx2\nHNAu40MHwXRjMhDv7Xyh9EN41NFQhmgo5cgIHo72/5VX/UlUjEbZlnSLtzfJG/Aq\nVzouPOk+gPQ5myLUw3qODHmuCsVIkRshh4PGCMaMWGVlixrEVyWQlLJAwO+1pBsd\nrO7qp6pl2wIa9IVh3yL5/Ivn6QKBgQC37cbKov30aOReadSYvwaj4Gw8sjGHzxlg\nXv7GxP5ya3obC/+ysxxXYFNabYvRLYfB37g3zlZ18iLwXE+MiqWVymKZYQT8gqJe\nTVkIiEcuMEw9+56k+fXcrIu8stxRbLM5Ww34xEfDdLy+m62kzE/8bfOBECVFWyIa\nbELpnYYVrQKBgAhfIic4Skg+jkW5gmR0/m17v3e/57NFA02ctDZLbP/9qhlpr3BQ\nBrKruaZvGe0Y/K6r540hqXJGhyu9hmYhI0vNpIvuijFTWWGOG2/AhS6YE1sDzP1Z\natcbnJ+QXODc+04dSGoQvexpOERbnj5dPouAvb3umBnaF09ZqKjdXqy5AoGAQIhK\noTpCxAgxZ/bVwqnzWHgbQch7nFzLWIa7+V2QsPCIrQiutDInlXiUhju9PK0u3pTx\n+5KHv2YvYAnjVzomEsIpigsCRt5GvbOBs36tCsWjWnP4G4Yf/0cC2DghGOh8kXkj\nE886JpfEyVOxvwp4Xebt8rCJgyYQdxjY+UJtkxkCgYBqv9VOw3kCqxPZ40Sq75gV\na/hkfLRHvGoau1I+LVD9cPPG1rpQG1yAsjjaobP9jzjtq6j/HWx5JAQ6WOc65nUv\nNe47LqW8p7XnYb+zTqToPY04MGlFxqhe8mmk8Zrd3ljO9dOtlcdSjl7g7MliUb8k\nDDOQEGQOvOo2EnRh8Yk9Lw==\n-----END PRIVATE KEY-----\n',
+  token: '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',
+  pr_number: '69'
+}
+
+fetchFromGithub(fromGit)
+
+return
+
+
+
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const nameToGreet = core.getInput("who-to-greet");
   console.log(`Hello ${nameToGreet}!`);
-  console.log(`sheetId ${core.getInput('sheetId')}!`);
-  console.log(`client_email ${core.getInput('client_email')}!`);
-  console.log(`token ${core.getInput('token')}!`);
-  console.log(`gitUrl ${core.getInput('gitUrl')}!`);
-  console.log(`pr_number ${core.getInput('pr_number')}!`);
+  console.log(`sheetId ${core.getInput("sheetId")}!`);
+  console.log(`client_email ${core.getInput("client_email")}!`);
+  console.log(`token ${core.getInput("token")}!`);
+  console.log(`gitUrl ${core.getInput("gitUrl")}!`);
+  console.log(`pr_number ${core.getInput("pr_number")}!`);
 
-  console.log('test 5');
-  const time = (new Date()).toTimeString();
+  console.log("test 5");
+  const time = new Date().toTimeString();
 
   fromGit = {
-    gitUrl: core.getInput('gitUrl'),
-    sheetId: core.getInput('sheetId'),
-    client_email: core.getInput('client_email'),
-    private_key: core.getInput('private_key'),
-    token: core.getInput('token'),
-    pr_number: core.getInput('pr_number'),
-  }
+    gitUrl: core.getInput("gitUrl"),
+    sheetId: core.getInput("sheetId"),
+    client_email: core.getInput("client_email"),
+    private_key: core.getInput("private_key"),
+    token: core.getInput("token"),
+    pr_number: core.getInput("pr_number"),
+  };
 
   // accessSpreadSheet(fromGit)
 
-  fetchFromGithub(fromGit)
+  fetchFromGithub(fromGit);
 
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
