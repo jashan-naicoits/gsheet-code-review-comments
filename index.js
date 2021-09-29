b8b7726d5f1d6c08fe765402a43faca371e12c3d
vineethvijayan <vineeth.v@naicoits.com>
2021-09-29 16:56:28 +0530

    removed code

--- index.js
+++ index.js
@@ -252,22 +252,6 @@ async function accessSpreadSheet(fromGit, result, sheetTitle, sheetPos) {
 }
 
 
-fromGit = {
-  gitUrl: 'https://api.github.com/repos/vineethvijayan/workflow/pulls',
-  sheetId: '1V2vATD8aplBrz_VfgwZxu48q4t9vHJXfAtPIVWzQSa8',
-  client_email: 'codereview@codereviewtracker-296806.iam.gserviceaccount.com',
-  private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCKQ4ZMWvKzJn32\nJOUXJF1HyUaK7M/7oW0Uk6In7+aX58LL/s0R/RURoA9XFBAv1r18zBWxspqjOcVk\niUurCIeLfUKsMprftDn6ELmtGqFLtr9SD/Hd67cQ5vuRCQvN2w3TZGSpRYa8eXHB\n8IznHWvIAKkQGG0TSPsJo9Jh3u5/lgQLhbH+qgDUZtpb/bx1DJULmVzbKsVijZaT\nC927lk6vMj/BWXHFvqkwpuaXp5YCAR8AQp3wBOpXz7EykpL0WwRBVeotlmXEJlqg\nD1g1Sw3Pp3neUcHi0BEhyDF7I2b6JWCmLfOBmph4BU0xJwoE6o768QZToibrkn3R\nSC1th9V1AgMBAAECggEABjomU/gLUvBFNLnSq5PSs7H9VbJ2vObh/87qZbwTTvzR\niYMvVJkTwth0+GoJ6e2qWR2Ooz3fuGxDV1TWWJyou3/nvykEUAD7qic+BZgxHYox\n3GNQHxDyYiDVFwzTlZuWaCfvCgSnSIPREM1huzhYVgiu6e+qXKJT1B+nEQthAylQ\nj8txR2PQ76mjdBU+bau91U6rd2DEvnrMe88ecKgWVl9aZ8baXv4chZTMrGlOK57u\niVGa2dvA1A1RPvi5EAgHC4DP9CHfNzIhqFImVVxZVqTY8ZrTvT2O+Z7nL2rNv5KU\nqAJmQkWBMpUdKDnOenGVEpSyJxrTKSCQz/6Hbq5cYQKBgQDAcQG83+YwBlT4Kxx2\nHNAu40MHwXRjMhDv7Xyh9EN41NFQhmgo5cgIHo72/5VX/UlUjEbZlnSLtzfJG/Aq\nVzouPOk+gPQ5myLUw3qODHmuCsVIkRshh4PGCMaMWGVlixrEVyWQlLJAwO+1pBsd\nrO7qp6pl2wIa9IVh3yL5/Ivn6QKBgQC37cbKov30aOReadSYvwaj4Gw8sjGHzxlg\nXv7GxP5ya3obC/+ysxxXYFNabYvRLYfB37g3zlZ18iLwXE+MiqWVymKZYQT8gqJe\nTVkIiEcuMEw9+56k+fXcrIu8stxRbLM5Ww34xEfDdLy+m62kzE/8bfOBECVFWyIa\nbELpnYYVrQKBgAhfIic4Skg+jkW5gmR0/m17v3e/57NFA02ctDZLbP/9qhlpr3BQ\nBrKruaZvGe0Y/K6r540hqXJGhyu9hmYhI0vNpIvuijFTWWGOG2/AhS6YE1sDzP1Z\natcbnJ+QXODc+04dSGoQvexpOERbnj5dPouAvb3umBnaF09ZqKjdXqy5AoGAQIhK\noTpCxAgxZ/bVwqnzWHgbQch7nFzLWIa7+V2QsPCIrQiutDInlXiUhju9PK0u3pTx\n+5KHv2YvYAnjVzomEsIpigsCRt5GvbOBs36tCsWjWnP4G4Yf/0cC2DghGOh8kXkj\nE886JpfEyVOxvwp4Xebt8rCJgyYQdxjY+UJtkxkCgYBqv9VOw3kCqxPZ40Sq75gV\na/hkfLRHvGoau1I+LVD9cPPG1rpQG1yAsjjaobP9jzjtq6j/HWx5JAQ6WOc65nUv\nNe47LqW8p7XnYb+zTqToPY04MGlFxqhe8mmk8Zrd3ljO9dOtlcdSjl7g7MliUb8k\nDDOQEGQOvOo2EnRh8Yk9Lw==\n-----END PRIVATE KEY-----\n',
-  token: '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',
-  pr_number: '69'
-}
-
-fetchFromGithub(fromGit)
-
-return
-
-
-
-
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput("who-to-greet");
